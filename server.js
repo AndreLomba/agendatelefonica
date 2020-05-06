d1f334e84246f07c644955e415412c303885fb88
André Lomba <landrelomba@gmail.com>
2020-05-06 20:07:33 +0000

    Criando o editar e o delet

--- server.js
+++ server.js
@@ -16,23 +16,40 @@ const pool = new Pool({
 
 
 server.use(express.json());
-
 // EXIBE O QUE EXISTE NA TABELA "AGENDA"
 // DEVEMOS USAR O ASYC AWAIT PARA QUE SÓ RETORNE APÓS O SELECT FOR FEITO NO BANCO
 server.get('/agenda', async function(request, response) {
-    const result = await pool.query('SELECT * FROM agenda');
+    const result = await pool.query('SELECT * FROM agenda ORDER BY id');
     return response.json(result.rows);
 });
 
 // INSERE OS DADOS RECEBIDOS POR POST NA TABELA "AGENDA"
 server.post('/agenda', async function(request, response) {
 
-    // PEGA OS VALORES DO POST QUE ESTÃO NO request.body
+    // PEGA OS VALORES QUE ESTÃO NO request.body
     const {nome,numero,favorito} = request.body;
 
     const sql = `INSERT INTO agenda (nome,numero,favorito) VALUES ($1, $2, $3)`;
     await pool.query(sql, [nome,numero,favorito]);
     return response.status(204).send();
 });
+
+// ATUALIZA UM PRODUTO PELO SEU ID
+server.put('/contato/:id', async (request, response) => {
+    const {id} = request.params;
+    const {nome,numero,favorito} = request.body;
+    const sql = `UPDATE agenda SET nome = $1, numero = $2, favorito = $3 WHERE id = $4`;
+    await pool.query(sql, [nome,numero,favorito,id]);
+    return response.status(204).send();
+})
+
+// DELETA UM PRODUTO PELO SEU ID
+server.delete('/contato/:id', async (request, response) => {
+    const {id} = request.params;
+    const sql = `DELETE FROM agenda WHERE id = $1`;
+    await pool.query(sql, [id]);
+    return response.status(204).send();
+})
+
 
 server.listen(process.env.PORT || 3000);
