eb6ab3d2d71512b98a24965ae539837e62da2e5f
André Lomba <landrelomba@gmail.com>
2020-05-06 00:07:37 +0000

    Alterações do crud

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', function(request, response) {
-    response.send('Mundo de André!');
-})
+server.use(express.json());
+
+const agenda = [
+    {nome:"Fernando", numero:"17991996644", favorito:"S"},
+    {nome:"Elieser", numero:"17991995599", favorito:"N"}
+]
+
+server.get('/agenda', function(request, response) {
+    response.json(agenda);
+});
+
+server.post('/agenda', function(request, response) {
+
+    const {nome,numero,favorito} = request.body;
+
+    agenda.push({nome, numero,favorito});
+    response.status(204).send();
+});
 
 server.listen(process.env.PORT || 3000);
